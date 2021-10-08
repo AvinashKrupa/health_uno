104d566c6018f2f0a72d7eb36e53decf9f58450b
Neel Patel <neel@cnetric.com>
2021-10-08 12:32:34 +0530

    FIxed HUNO-343: Logout api in admin panel

--- src/admin/components/header/index.jsx
+++ src/admin/components/header/index.jsx
@@ -4,6 +4,7 @@ import logoicon from "../../assets/images/logo-small.png";
 import avatar from "../../assets/images/avatar-01.jpg";
 import Dropdown from "react-bootstrap/Dropdown";
 import $ from "jquery";
+import { fetchApi } from "../../../_utils/http-utils";
 
 
 class Header extends Component {
@@ -40,6 +41,23 @@ class Header extends Component {
         require('../../assets/css/font-awesome.min.css')
         require('../../assets/css/style.css')
         // }
+
+    }
+
+    handleLogout = async() => {
+        try {
+            let result = await fetchApi({
+                url: "v1/auth/logout",
+                method: "POST",
+            })
+            if (result.status === 200) {
+                toast.success(result.message)
+                localStorage.clear();
+                this.props.history.push("/login")
+            }
+        } catch (e) {
+            console.log("error>>", e)
+        }
 
     }
 
@@ -202,7 +220,7 @@ class Header extends Component {
                                 </Dropdown.Item>
                                 <Dropdown.Item href="/profile"> My Profile</Dropdown.Item>
                                 <Dropdown.Item href="/settings">Settings</Dropdown.Item>
-                                <Dropdown.Item href="/login">Logout</Dropdown.Item>
+                                <Dropdown.Item onClick={this.handleLogout}>Logout</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     </li>
