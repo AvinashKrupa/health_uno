9d809cd64912aa6eaf2520d7509107e6fcac520f
Neel Patel <neel@cnetric.com>
2021-10-19 18:13:38 +0530

    Fixed HUNO-143: Dynamic values in dashboard

--- src/admin/components/dashboard/tableDoctor/index.jsx
+++ src/admin/components/dashboard/tableDoctor/index.jsx
@@ -1,74 +1,50 @@
-import React, { Component } from 'react';
-import { Table } from 'antd';
-import 'antd/dist/antd.css';
-import IMG01 from '../../../assets/images/doctors/doctor-thumb-01.jpg';
-import IMG02 from '../../../assets/images/doctors/doctor-thumb-02.jpg';
-import IMG03 from '../../../assets/images/doctors/doctor-thumb-03.jpg';
-
-
+import React, { Component } from "react";
+import { Table } from "antd";
+import "antd/dist/antd.css";
+import {
+  renderChips,
+  renderName,
+  renderText,
+} from "../../../../_utils/data-table-utils";
 
 class TableDoctor extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            data:[
-                {id:1,image:IMG01,Name:"Dr. Ruby Perrin",Speciality:"Dental",Earned:"$3200.00"},
-                {id:2,image:IMG02,Name:"Dr. Darren Elde",Speciality:"Dental",Earned:"$3100.00"},
-                {id:3,image:IMG03,Name:"Dr. Deborah Angel",Speciality:"Cardiology",Earned:"$4000.00"},
-               
-                
-            ]
-        }
-    }
   render() {
-      const { data } = this.state
-
-      const columns = [
-          {
-            title: 'Doctor Name',
-            dataIndex: 'Name',
-            render: (text, record) => (            
-                <h2 className="table-avatar">
-                  <a href="/profile" className="avatar avatar-sm mr-2"><img alt="" src={record.image} /></a>
-                  <a href="/profile">{text}</a>
-                </h2>
-              ), 
-          },
-          {
-            title: 'Speciality',
-            dataIndex: 'Speciality'
-          },
-          {
-            title: 'Earned',
-            dataIndex: 'Earned',
-          },
-          {
-            title: 'Reviews',
-            render: () => (
-                  <> 
-                <i className="fe fe-star text-warning"></i>
-              <i className="fe fe-star text-warning"></i>
-              <i className="fe fe-star text-warning"></i>
-              <i className="fe fe-star text-warning"></i>
-              <i className="fe fe-star-o text-secondary"></i>
-                </>
-             ),
-          },
-      ]
+    const columns = [
+      {
+        title: "Doctor Name",
+        dataIndex: "Name",
+        render: (text, record) => renderName(record, "Dr", "", true),
+      },
+      {
+        title: "Speciality",
+        dataIndex: "Speciality",
+        render: (text, record) => renderChips(record.qualif.specl),
+      },
+      {
+        title: "Fees (Rupees)",
+        render: (text, record) => renderText(record.qualif.fee),
+      },
+      {
+        title: "Department",
+        render: (text, record) => renderText(record.qualif.dept_id.title),
+      },
+    ];
     return (
-       <div>
-            <Table className="table-striped"
-                  style = {{overflowX : 'auto'}}
-                  columns={columns}                 
-                  // bordered
-                  dataSource={data}
-                  rowKey={record => record.id}
-                  pagination={false} 
-                />
-       </div>
-      
-    )
+      <div>
+        <Table
+          className="table-striped"
+          style={{ overflowX: "auto" }}
+          columns={columns}
+          // bordered
+          dataSource={this.props.doctors}
+          rowKey={(record) => record.id}
+          pagination={{
+            pageSize: 5,
+          }}
+        />
+      </div>
+    );
   }
-};
+}
 
 export default TableDoctor;
--- src/admin/components/dashboard/tablePatientList/index.jsx
+++ src/admin/components/dashboard/tablePatientList/index.jsx
@@ -1,67 +1,54 @@
-import React, { Component } from 'react';
-import { Table } from 'antd';
-import 'antd/dist/antd.css';
-import IMG01 from '../../../assets/images/doctors/doctor-thumb-01.jpg';
-import IMG02 from '../../../assets/images/doctors/doctor-thumb-02.jpg';
-import IMG03 from '../../../assets/images/doctors/doctor-thumb-03.jpg';
-
-
+import React, { Component } from "react";
+import { Table } from "antd";
+import "antd/dist/antd.css";
+import {
+  renderName,
+  sorterNumber,
+  sorterText,
+} from "../../../../_utils/data-table-utils";
 
 class TablePatientsList extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            data:[
-                {id:1,image:IMG01,Name:"Dr. Ruby Perrin",Phone:"8286329170",LastVisit:"20 Oct 2019",Paid:'$100.00'},
-                {id:2,image:IMG02,Name:"Dr. Darren Elde",Phone:"2077299974",LastVisit:"22 Oct 2019",Paid:'$200.00'},
-                {id:3,image:IMG03,Name:"Dr. Deborah ",Phone:"2607247769",LastVisit:"21 Oct 2019",Paid:'$250.00'},
-               
-                
-            ]
-        }
-    }
   render() {
-      const { data } = this.state
-
-      const columns = [
-          {
-            title: 'Doctor Name',
-            dataIndex: 'Name',
-            render: (text, record) => (            
-                <h2 className="table-avatar">
-                  <a href="/profile" className="avatar avatar-sm mr-2"><img alt="" src={record.image} /></a>
-                  <a href="/profile">{text}</a>
-                </h2>
-              ), 
-          },
-          {
-            title: 'Phone',
-            dataIndex: 'Phone'
-          },
-          {
-            title: 'LastVisit',
-            dataIndex: 'LastVisit',
-          },
-          {
-            title: 'Paid',
-            dataIndex: 'Paid',
-          },
-          
-      ]
+    const columns = [
+      {
+        title: "Patient Name",
+        dataIndex: "Name",
+        render: (text, record) => renderName(record, "", "", true),
+      },
+      {
+        title: "Phone",
+        dataIndex: "Phone",
+        render: (text, record) => {
+          return <p>{record.user_id.mobile_number}</p>;
+        },
+      },
+      {
+        title: "Height (Feet)",
+        dataIndex: "height",
+        sorter: (a, b) => sorterText(a.height, b.height),
+      },
+      {
+        title: "Weight (Kg)",
+        dataIndex: "weight",
+        sorter: (a, b) => sorterNumber(a.weight, b.weight),
+      },
+    ];
     return (
-       <div>
-            <Table className="table-striped"
-                  style = {{overflowX : 'auto'}}
-                  columns={columns}                 
-                  // bordered
-                  dataSource={data}
-                  rowKey={record => record.id}
-                  pagination={false} 
-                />
-       </div>
-      
-    )
+      <div>
+        <Table
+          className="table-striped"
+          style={{ overflowX: "auto" }}
+          columns={columns}
+          // bordered
+          dataSource={this.props.patients}
+          rowKey={(record) => record.id}
+          pagination={{
+            pageSize: 5,
+          }}
+        />
+      </div>
+    );
   }
-};
+}
 
 export default TablePatientsList;
