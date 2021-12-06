ddae5ed06d4eb4ee0a5711deb6c9165ba5121dc5
paramveer.singh <paramveer.singh@creativeclaritycreations.com>
2021-12-06 09:53:27 +0530

    updated: base url updated to uat

--- src/socketIO/SocketManager.js
+++ src/socketIO/SocketManager.js
@@ -1,6 +1,6 @@
 
 const { io } = require("socket.io-client");
-const URL = "https://dev.healthuno.com:6003";
+const URL = "https://uat.healthuno.com:6003";
 
 // const URL = "http://localhost:3007";
 const options = {
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     // global app config object
     config: JSON.stringify({
       // apiUrl: 'http://localhost:3005/',
-      apiUrl: "https://dev.healthuno.com:6002/",
+      apiUrl: "https://uat.healthuno.com:6002/",
       imageapiUrl: "",
       publicPath: "/",
     }),
