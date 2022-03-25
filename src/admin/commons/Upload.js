94f3d9940f4c070b7dfc9320e22259ebf81517c8
Yatish Agrawal <yatish.agrawal@mqspectrum.com>
2022-03-25 21:11:04 +0530

    merged

--- src/admin/commons/Upload.js
+++ src/admin/commons/Upload.js
@@ -29,7 +29,7 @@ class UploadImage extends Component {
     }
     bodyFormData.append("type", "profile");
     this.uploadImageWithData(
-      "https://api.healthuno.com/v1/fileUpload",
+      "https://uat.healthuno.com:6002/v1/fileUpload",
       bodyFormData
     )
       .then((response) => {})
