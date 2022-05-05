175f8c7695552a69becdb60f16126d9da5d7e09e
Yatish Agrawal <yatish.agrawal@mqspectrum.com>
2022-05-05 14:01:50 +0530

    fixed url

--- src/_utils/constants.js
+++ src/_utils/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'https://dev.healthuno.com:6002';
-export const UPLOAD_FILE_URL = BASE_URL+'/v1/fileUpload';
+const BASE_URL = "https://uat.healthuno.com:6002";
+export const UPLOAD_FILE_URL = BASE_URL + "/v1/fileUpload";
